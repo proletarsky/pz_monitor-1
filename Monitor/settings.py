e0f23a7379be40577e44dfed982ff750a8625c18
Roman <Roman@mail.ru>
2021-05-21 08:20:10 +0300

    change settings

--- Monitor/settings.py
+++ Monitor/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'Monitor.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'aa_pzmonitor',
+        'NAME': 'pzmonitor',
         'USER': 'djangouser',
         'PASSWORD': 'password',
         'HOST': '127.0.0.1',
